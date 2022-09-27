c7a262e916daa9a20c4e3a35227f5f23779a8ff3
tamal <tamal8730@gmail.com>
2022-09-27 10:32:11 +0530

    initial: initial commit

---
+++ src/main/java/com/github/tamal8730/cotext/CotextApplication.java
@@ -0,0 +1,13 @@
+package com.github.tamal8730.cotext;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CotextApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CotextApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/github/tamal8730/cotext/CotextApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.tamal8730.cotext;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CotextApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
